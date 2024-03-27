c36b1e47f61dc391db469d59843a9dbd08dae103
Gr3ch4 <grisha.salmin.2002@mail.ru>
2024-03-27 20:25:06 +0300

    add json-server, add useFetch hook,

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import Lamp from './img/lamp.svg'
-import Table from './img/table.svg'
-import Chair from './img/chair.svg'
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
-
+import {useFetch} from './hooks/useFetch'
 import './App.css';
+import lamp from './img/lamp.svg'
+import chair from './img/chair.svg'
+import table from './img/table.svg'
+
 
 // import required modules
 import { Navigation } from 'swiper/modules';
+
+const images = {
+  lamp: lamp,
+  chair: chair,
+  table: table
+};
 
 const onSpeech = (e) => {
   const speechWord = document.querySelector('.swiper-slide-active b').textContent
@@ -21,23 +28,21 @@ const onSpeech = (e) => {
     window.speechSynthesis.speak(speech);
 }
 
-const words = [
-  ['lamp', 'лампа', Lamp],
-  ['table', 'стол', Table],
-  ['chair', 'стул', Chair]
-];
 
 export default function App() {
+  const { data, isPending, error} = useFetch('http://localhost:8000/words')
   return (
     <div className='App'>
       <button onClick={onSpeech} className='speech-btn'></button>
       <Swiper loop={true} navigation={true} modules={[Navigation]} className="swiper">
-        {words.map(word => (
-          <SwiperSlide key={word[0]}>
+        {error && <p>{error}</p>}
+        {isPending && <p>Loading...</p>}
+        {data && data.map(word => (
+          <SwiperSlide key={word.id}>
             <div className="slide-content">
-              <img src={word[2]} className='swiper-img'/>
-              <b>{word[0]}</b>
-              <i>{word[1]}</i>
+              <img src={images[word.word]} className='swiper-img'/>
+              <b>{word.word}</b>
+              <i>{word.translation}</i>
             </div>
           </SwiperSlide>
         ))}
